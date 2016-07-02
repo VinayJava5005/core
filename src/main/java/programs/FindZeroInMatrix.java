bec4e2c84e475992951321363ce7f0c769fb9c27
Pradeep Arya <parya@xebia.com>
2016-07-02 20:01:17 +0530

    findzeroinmatrix program added

--- src/main/java/programs/FindZeroInMatrix.java
+++ src/main/java/programs/FindZeroInMatrix.java
@@ -5,8 +5,57 @@ package programs;
  */
 public class FindZeroInMatrix {
 
+    private static int countZero(int[][] matrix) {
+        int count = 0;
+
+        int row = 0;
+        int col = 0;
+        while (row < 4 && col < 3) {
+            if (matrix[row][col++] == 0) {
+                count++;
+            } else {
+                row++;
+                col = 0;
+            }
+
+            if (col == 3) {
+                row++;
+                col = 0;
+            }
+
+        }
+
+        return count;
+    }
+
+    private static int getZeroCount(int[][] matrix, int row) {
+
+        int count = 0;
+        for (int index = 0; index < row; index++) {
+            count += countZeroRecursive(matrix[index], 0, matrix[index].length - 1);
+        }
+
+        return count;
+
+    }
+
+    private static int countZeroRecursive(int[] matrix, int startIndex, int lastIndex) {
+        int count = 0;
+        if ((startIndex <= lastIndex) && (startIndex < matrix.length)) {
+            int mid = (startIndex + lastIndex) / 2;
+            if (matrix[mid] == 0) {
+                count += (mid - startIndex + 1);
+                count += countZeroRecursive(matrix, mid + 1, lastIndex);
+            } else {
+                count += countZeroRecursive(matrix, startIndex, mid - 1);
+            }
+        }
+        return count;
+    }
 
     public static void main(String[] args) {
-
+        int[][] matrix = {{0, 0, 1}, {0, 0, 1}, {0, 1, 1}, {0, 1, 1}};
+        System.out.println(countZero(matrix));
+        System.out.println(getZeroCount(matrix, 4));
     }
 }
